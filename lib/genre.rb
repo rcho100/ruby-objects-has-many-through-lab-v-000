2713f58af334593be8ddd9b26d80c62316600dd2
Richard Cho <richardcho100@gmail.com>
2018-05-27 05:14:54 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,12 @@
-require 'pry'
 class Genre
   attr_accessor :name, :artists
 
   @@all = []
 
+  def self.all
+    @@all
+  end
+  
   def initialize(name)
     @name = name
     @@all << self
@@ -14,18 +17,10 @@ class Genre
   end
 
   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
+    Song.all.select {|song| song.genre == self}
   end
 
   def artists
-    self.songs.collect do |song|
-      song.artist 
-    end
-  end
-
-  def self.all
-    @@all
+    self.songs.collect {|song| song.artist}
   end
 end
