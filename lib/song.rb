30fe0a9653f8fb1f2cb35d8407de5c27d6b0ac86
Richard Cho <richardcho100@gmail.com>
2018-05-27 05:13:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,14 @@ class Song
 
   @@all = []
 
+  def self.all
+    @@all
+  end
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@all << self
-  end
-
-
-  def self.all
-    @@all
   end
 end
